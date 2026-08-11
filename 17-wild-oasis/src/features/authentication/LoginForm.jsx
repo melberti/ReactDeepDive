2c03b8c8af91ddfa4d012c7ee875a622112836a0
Melanie Berti <melberti@gmail.com>
2026-08-11 15:35:42 -0400

    bugfix

--- 17-wild-oasis/src/features/authentication/LoginForm.jsx
+++ 17-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("mel@mel.com");
-  const [password, setPassword] = useState("cupcakes");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isAuthenticating } = useLogin();
 
   function handleSubmit(e) {
